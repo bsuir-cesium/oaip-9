1d6cbf577e86d2ef763cb75a930c5bd57ce27236
MarkCesium <danikhrenkov29@gmail.com>
2025-02-14 17:36:32 +0300

    (draft) Trying to merge binary search in one

--- src/main.dpr
+++ src/main.dpr
@@ -1,20 +1,20 @@
 program main;
 
-// {$mode DELPHI}
+{$mode DELPHI}
 {$APPTYPE CONSOLE}
 
 uses
   SysUtils;
 
 const
-  len_array = 100;
+  len_array = 1000;
   len_name = 20;
   random_range = 326;
 
 type
   TRecord = Record
     value: integer;
-    name: string;
+    name: string[len_name];
   end;
 
   TArr = array [1 .. len_array] of TRecord;
@@ -51,54 +51,29 @@ var
 begin
   firstIndex := 1;
   lastIndex := len_array;
-  if byName then
-    while firstIndex < lastIndex do
-    begin
-      for i := firstIndex to lastIndex - 1 do
-        if arr[i].name > arr[i + 1].name then
-        begin
-          tempValue := arr[i];
-          arr[i] := arr[i + 1];
-          arr[i + 1] := tempValue;
-        end;
-
-      for i := lastIndex downto firstIndex + 1 do
-        if arr[i].name < arr[i - 1].name then
-        begin
-          tempValue := arr[i];
-          arr[i] := arr[i - 1];
-          arr[i - 1] := tempValue;
-        end;
-
-      firstIndex := firstIndex + 1;
-      lastIndex := lastIndex - 1;
-    end
-  else
-    while firstIndex < lastIndex do
-    begin
-      for i := firstIndex to lastIndex - 1 do
-        if arr[i].value > arr[i + 1].value then
-        begin
-          tempValue := arr[i];
-          arr[i] := arr[i + 1];
-          arr[i + 1] := tempValue;
-        end;
-
-      for i := lastIndex downto firstIndex + 1 do
-        if arr[i].value < arr[i - 1].value then
-        begin
-          tempValue := arr[i];
-          arr[i] := arr[i - 1];
-          arr[i - 1] := tempValue;
-        end;
-
-      firstIndex := firstIndex + 1;
-      lastIndex := lastIndex - 1;
+  while firstIndex < lastIndex do
+  begin
+    for i := firstIndex to lastIndex - 1 do
+      if ((arr[i].name > arr[i + 1].name) and byName) or ((arr[i].value > arr[i + 1].value) and not byName) then
+      begin
+        tempValue := arr[i];
+        arr[i] := arr[i + 1];
+        arr[i + 1] := tempValue;
+      end;
+    for i := lastIndex downto firstIndex + 1 do
+      if ((arr[i].name < arr[i - 1].name) and byName) or ((arr[i].value < arr[i - 1].value) and not byName) then
+      begin
+        tempValue := arr[i];
+        arr[i] := arr[i - 1];
+        arr[i - 1] := tempValue;
+      end;
+    firstIndex := firstIndex + 1;
+    lastIndex := lastIndex - 1;
     end;
 end;
 
-function BinSearchByName(const arr: TArr; const name: string;
-  leftIndex, rightIndex: integer; var counter: integer): integer;
+function BinSearch(const arr: TArr; const criteria: Variant;
+  leftIndex, rightIndex: integer; var counter: integer; const byName: boolean): integer;
 var
   middleIndex: integer;
   search: boolean;
@@ -107,20 +82,21 @@ begin
   while search do
   begin
     counter := counter + 1;
-    if (leftIndex = rightIndex) and (arr[leftIndex].name <> name) then
+    if (leftIndex = rightIndex) and (((arr[leftIndex].name <> criteria) and byName)
+      or ((arr[leftIndex].value <> criteria) and not byName)) then
     begin
-      BinSearchByName := -1;
+      BinSearch := -1;
       search := False;
     end
     else
     begin
       middleIndex := (leftIndex + rightIndex) div 2;
-      if arr[middleIndex].name = name then
+      if ((arr[middleIndex].name = criteria) and byName) or ((arr[middleIndex].value = criteria) and not byName) then
       begin
-        BinSearchByName := middleIndex;
+        BinSearch := middleIndex;
         search := False;
       end
-      else if arr[middleIndex].name < name then
+      else if ((arr[middleIndex].name < criteria) and byName) or ((arr[middleIndex].value < criteria) and not byName) then
         leftIndex := middleIndex + 1
       else
         rightIndex := middleIndex;
@@ -191,7 +167,7 @@ end;
 var
   arr: TArr;
   indexes: TIndexes;
-  index, counter, searchValue, i: integer;
+  index, counter, searchValue, i, j: integer;
   searchString: string;
   searchFlag: boolean;
 
@@ -207,14 +183,15 @@ begin
     write('Enter search string: ');
     readln(searchString);
     counter := 0;
-    index := BinSearchByName(arr, searchString, 1, len_array, counter);
+    index := BinSearch(arr, searchString, 1, len_array, counter, True);
     if (index = -1) then
       writeln('Not found, try again')
     else
     begin
       writeln('(index: ', index, ', value: ', arr[index].value, ', name: ',
         arr[index].name, ', counter: ', counter, ')');
-      writeln;
+      writeln('Press Enter...');
+      readln;
       searchFlag := False;
     end;
   end;
@@ -225,22 +202,55 @@ begin
   searchFlag := True;
   while searchFlag do
   begin
-    write('Enter search string: ');
-    readln(searchValue);
-    counter := 0;
-    indexes := BinSearchByValue(arr, searchValue, 1, len_array, counter);
-    if (indexes[0] = -1) then
-      writeln('Not found, try again')
-    else
-    begin
-      for i := low(indexes) to high(indexes) do
-        writeln('(value: ', arr[indexes[i]].value, ', name: ',
-          arr[indexes[i]].name, ')');
-      writeln;
-      searchFlag := False;
-    end;
+      write('Enter search value: ');
+      readln(searchValue);
+      index := BinSearch(arr, searchValue, 1, len_array, counter, False);
+
+      if (index = -1) then
+      begin
+        writeln('Not found, try again')
+      end
+      else
+      begin
+        setLength(indexes, 1);
+        indexes[0] := index;
+
+        i := index + 1;
+        j := 2;
+        while (searchFlag) and (i <= len_array) do
+          if arr[i].value = searchValue then
+          begin
+            setLength(indexes, j);
+            indexes[j - 1] := i;
+            Inc(i);
+            Inc(counter);
+            Inc(j);
+          end
+          else
+            searchFlag := False;
+        
+        searchFlag := True;
+        i := index - 1;
+        while (searchFlag) and (i >= 1) do
+          if arr[i].value = searchValue then
+          begin
+            setLength(indexes, i);
+            indexes[j - 1] := i;
+            Dec(i);
+            Inc(counter);
+            Inc(j);
+          end
+          else
+            searchFlag := False;
+
+        searchFlag := False;
+      end;
   end;
 
+  for i := Low(indexes) to High(indexes) do
+  begin
+    writeln('(index: ', indexes[i], ', value: ', arr[indexes[i]].value, ', name: ',
+        arr[indexes[i]].name, ', counter: ', counter, ')');
+  end;
   readln;
-
 end.
